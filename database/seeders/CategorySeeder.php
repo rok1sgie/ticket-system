dbc8c27950c91fa3e4fc47306b1957f693e7f3bc
rok1sgie <rokas.giedraitis@stud.svako.lt>
2026-05-19 10:45:11 +0300

    Seeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,16 +2,22 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class CategorySeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        $categories = [
+            'Techninė įranga',
+            'Programinė įranga',
+            'Tinklas',
+            'Prieiga',
+        ];
+
+        foreach ($categories as $category) {
+            Category::firstOrCreate(['name' => $category]);
+        }
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,16 +2,39 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        User::firstOrCreate(
+            ['email' => 'rokas.giedraitis@stud.svako.lt'],
+            [
+                'name' => 'Administratorius',
+                'password' => Hash::make('rokas123'),
+                'role' => 'admin',
+            ]
+        );
+
+        User::firstOrCreate(
+            ['email' => 'support@stud.svako.lt'],
+            [
+                'name' => 'Support darbuotojas',
+                'password' => Hash::make('rokas123'),
+                'role' => 'support',
+            ]
+        );
+
+        User::firstOrCreate(
+            ['email' => 'Edvinas@stud.svako.lt'],
+            [
+                'name' => 'Paprastas vartotojas',
+                'password' => Hash::make('rokas123'),
+                'role' => 'user',
+            ]
+        );
     }
 }
