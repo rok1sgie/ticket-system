9648358061d54838b5dd6146f739e5ec9ae229b3
rok1sgie <rokas.giedraitis@stud.svako.lt>
2026-05-20 09:20:22 +0300

    Report Controller

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Ticket;
+use Barryvdh\DomPDF\Facade\Pdf;
+use Illuminate\Http\Response;
 
 class ReportController extends Controller
 {
-    //
+    public function activeTickets(): Response
+    {
+        $tickets = Ticket::with(['user', 'category'])
+            ->whereNotIn('status', [Ticket::STATUS_RESOLVED, Ticket::STATUS_CLOSED])
+            ->latest()
+            ->get();
+
+        $pdf = Pdf::loadView('reports.active_tickets', compact('tickets'));
+
+        return $pdf->download('aktyvus-bilietai.pdf');
+    }
 }
