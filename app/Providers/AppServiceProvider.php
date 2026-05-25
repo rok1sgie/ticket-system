546a254af9ebc97ac7a41bfd2946ce5ff9d139d1
rok1sgie <rokas.giedraitis@stud.svako.lt>
2026-05-25 08:50:34 +0300

    Kategorijų matymo apribojimas

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,19 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        Gate::define('admin', function ($user) {
+            return $user->role === 'admin';
+        });
     }
 }
